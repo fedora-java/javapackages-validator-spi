ba91277b9237f18585692f981318f07cbfe8faa1
Marián Konček <marian.koncek@mailbox.org>
2023-12-04 15:33:37 +0100

    Add module-info

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+/**
+ * SPI module
+ */
+module org.fedoraproject.javapackages.validator.spi {
+    exports org.fedoraproject.javapackages.validator.spi;
+    requires transitive io.kojan.javadeptools.rpm;
+}
