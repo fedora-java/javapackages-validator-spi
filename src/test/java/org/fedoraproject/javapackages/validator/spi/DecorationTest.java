8bada00a060796d5cac1bc537914aa8b1709ac1d
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-03-06 14:14:27 +0100

    Implement more tests

--- src/test/java/org/fedoraproject/javapackages/validator/spi/DecorationTest.java
+++ src/test/java/org/fedoraproject/javapackages/validator/spi/DecorationTest.java
@@ -2,6 +2,8 @@ package org.fedoraproject.javapackages.validator.spi;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Optional;
+
 import org.fedoraproject.javapackages.validator.spi.Decoration.Color;
 import org.fedoraproject.javapackages.validator.spi.Decoration.Modifier;
 import org.junit.jupiter.api.Test;
@@ -10,6 +12,34 @@ import org.junit.jupiter.api.Test;
  * @author Mikolaj Izdebski
  */
 public class DecorationTest {
+
+    @Test
+    void testConstructorWithColorAndModifiers() {
+        Decoration d = new Decoration(Color.red, Modifier.bold, Modifier.underline);
+        assertEquals(Optional.of(Color.red), d.color());
+        assertArrayEquals(new Modifier[] { Modifier.bold, Modifier.underline }, d.modifiers());
+    }
+
+    @Test
+    void testConstructorWithOnlyModifiers() {
+        Decoration d = new Decoration(Modifier.bright);
+        assertEquals(Optional.empty(), d.color());
+        assertArrayEquals(new Modifier[] { Modifier.bright }, d.modifiers());
+    }
+
+    @Test
+    void testDefaultNoneDecoration() {
+        assertEquals(Optional.empty(), Decoration.NONE.color());
+        assertArrayEquals(new Modifier[] {}, Decoration.NONE.modifiers());
+    }
+
+    @Test
+    void testModifiersReturnsCopy() {
+        Decoration d = new Decoration(Color.blue, Modifier.bold);
+        Modifier[] mods = d.modifiers();
+        mods[0] = Modifier.underline;
+        assertArrayEquals(new Modifier[] { Modifier.bold }, d.modifiers());
+    }
 
     @Test
     void testEquality() {
