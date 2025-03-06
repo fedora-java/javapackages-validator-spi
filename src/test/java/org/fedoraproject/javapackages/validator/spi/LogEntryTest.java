8bf7274f1ec1ea220279c46bbbe417baf9113331
Mikolaj Izdebski <mizdebsk@redhat.com>
2025-03-06 14:14:27 +0100

    Implement more tests

--- src/test/java/org/fedoraproject/javapackages/validator/spi/LogEntryTest.java
+++ src/test/java/org/fedoraproject/javapackages/validator/spi/LogEntryTest.java
@@ -10,6 +10,37 @@ import org.junit.jupiter.api.Test;
 class LogEntryTest {
 
     @Test
+    void testConstructor() {
+        Decorated obj1 = Decorated.rpm("foo-bar");
+        Decorated obj2 = Decorated.struct("struct {}");
+        LogEntry log = new LogEntry(LogEvent.info, "Test pattern {0} {1}", obj1, obj2);
+
+        assertEquals(LogEvent.info, log.kind());
+        assertEquals("Test pattern {0} {1}", log.pattern());
+        assertArrayEquals(new Decorated[] { obj1, obj2 }, log.objects());
+    }
+
+    @Test
+    void testObjectsReturnsCopy() {
+        Decorated obj = Decorated.outer("foo");
+        LogEntry log = new LogEntry(LogEvent.warn, "Test pattern {0}...", obj);
+        log.objects()[0] = Decorated.actual("ACT");
+        assertArrayEquals(new Decorated[] { obj }, log.objects());
+    }
+
+    @Test
+    void testFactoryMethods() {
+        Decorated obj = Decorated.outer("lorem ipsum....");
+        assertEquals(new LogEntry(LogEvent.debug, "Pattern {0}...", obj), LogEntry.debug("Pattern {0}...", obj));
+        assertEquals(new LogEntry(LogEvent.skip, "Pattern {0}...", obj), LogEntry.skip("Pattern {0}...", obj));
+        assertEquals(new LogEntry(LogEvent.pass, "Pattern {0}...", obj), LogEntry.pass("Pattern {0}...", obj));
+        assertEquals(new LogEntry(LogEvent.info, "Pattern {0}...", obj), LogEntry.info("Pattern {0}...", obj));
+        assertEquals(new LogEntry(LogEvent.warn, "Pattern {0}...", obj), LogEntry.warn("Pattern {0}...", obj));
+        assertEquals(new LogEntry(LogEvent.fail, "Pattern {0}...", obj), LogEntry.fail("Pattern {0}...", obj));
+        assertEquals(new LogEntry(LogEvent.error, "Pattern {0}...", obj), LogEntry.error("Pattern {0}...", obj));
+    }
+
+    @Test
     void testEquality() {
         LogEntry log1 = new LogEntry(LogEvent.pass, "Same pattern {0}...", Decorated.outer("foo"));
         LogEntry log2 = new LogEntry(LogEvent.pass, "Same pattern {0}...", Decorated.outer("foo"));
